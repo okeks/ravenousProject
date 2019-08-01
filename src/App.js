cb854c1367736059832740b93e49e294a636a7d9
Princewill <okechukwuprincewillw@gmail.com>
2019-08-01 10:28:19 +0100

    using this.props pt1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import logo from "./logo.svg";
 import "./App.css";
 import BusinessList from "../src/components/BusinessList/BusinessList";
 import SearchBar from "../src/components/SearchBar/SearchBar";
+
+let business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90
+};
+const businesses = [business, business, business, business, business, business];
+
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList business={businesses} />
     </div>
   );
 }
