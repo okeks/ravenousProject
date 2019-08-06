1832cee35cc68d2e6f84ebf8c0e812b88fa7f64b
Princewill <okechukwuprincewillw@gmail.com>
2019-08-06 12:52:22 +0100

    third part of Ravenous

--- src/App.js
+++ src/App.js
@@ -18,13 +18,18 @@ let business = {
 };
 const businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList business={businesses} />
-    </div>
-  );
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term},${location},${sortBy}`);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList business={businesses} />
+      </div>
+    );
+  }
 }
 export default App;
